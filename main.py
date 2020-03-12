acdb3a955c5eb390403b76a637cf3c00b6f84623
ivekhov <ivekhov@gmail.com>
2020-03-12 07:30:00 +0000

    added quiscort class

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from sorting import quicksort
+from sorting.quicksort import QuickSort
 
-quicksort.sort()
+
+def main():
+    testsort = QuickSort(arr)
+    arr = [1000, 42, 0, -99, 31, 17, 5, -2]
+   
+    testsort.myprint()
+    testsort.sort(0, len(arr)-1)
+    testsort.myprint()
+
+
+if __name__ == "__main__":
+    main()
--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -1,14 +1,29 @@
-def partition():
-    pass
+class QuickSort:
 
+    def __init__(self, arr):
+        self.array = arr
+    
+    def swap(self, a, b):
+        temp = self.array[a]
+        self.array[a] = self.array[b]
+        self.array[b] = temp
+    
+    def myprint(self):
+        for item in self.array:
+            print('{}'.format(item), end=" ")
+        print("\n")
 
-def sort():
-    print(42)
+    def partition(self, left, right):
+        pivot = self.array[right]
+        index = left - 1
+        for j in range(left, right + 1):
+            if self.array[j] <= pivot:
+                index += 1
+                self.swap(index, j)
+        return index
 
-
-def main():
-    sort()
-
-
-if __name__ == "__main__":
-    main()
+    def sort(self, left, right):
+        if left >= right: return
+        center = self.partition(left, right)
+        self.sort(left, center-1);
+        self.sort(center+1, right)
